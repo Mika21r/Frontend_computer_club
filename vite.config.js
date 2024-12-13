2ed7d4cc4e3016abf0bd5795074df2a34e89f4be
Mika21r <mih.kirill0318@gmail.com>
2024-12-13 14:11:54 +0300

    check commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Frontend_computer_club/",
+  base: "/kursovay_game_club_front-main/",
   plugins: [react()],
 })
